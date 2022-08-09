1be0bf863d14a2f216095cc653419511cfc33339
Daniel Alves <estudos.daniel@yahoo.com>
2022-08-09 19:34:44 -0300

    Urna Eletronica

---
+++ urna/urnaEletronica/etapas.js
@@ -0,0 +1,60 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'LUCAS RAFAEL MENDES',
+                partido: 'BOIOLAS',
+                fotos: [
+                    {url:'38111.jpg', legenda: 'Vereador'}
+                ]
+            },
+            
+            {
+                numero: '77222',
+                nome: 'KENY KENY',
+                partido: 'VIADOS',
+                fotos:[
+                    {url:'77222.jpg',legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '24244',
+                nome: 'JOJO TODINHO',
+                partido: 'GOR',
+                fotos:[
+                    {url:'77222.jpg',legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'NONÓ',
+                partido: 'ABC',
+                vice: 'DEAN',
+                fotos: [
+                    {url:'99.jpg', legenda: 'Prefeito'},
+                    {url:'99_2.png', legenda: 'Vice-prefeito', small: true}
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'JOJÓ',
+                partido: 'PSDCVF',
+                vice: 'MAIARA',
+                fotos: [
+                    {url:'84.jpg', legenda:'Prefeito'},
+                    {url:'84_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+
+    }
+];
---
+++ urna/urnaEletronica/script.js
@@ -0,0 +1,86 @@
+let seuVotopara = document.querySelector('.d-1-1 span');
+let cargo = document.querySelector('.d-1-2 span');
+let descricao = document.querySelector('.d-1-4');
+let aviso = document.querySelector('.d-2');
+let lateral = document.querySelector('.d-1-right');
+let numeros = document.querySelector('.d-1-3');
+
+
+let etapaAtual = 0;
+let numero = '';
+
+function comecarEtapa() {
+    let etapa = etapas[etapaAtual];
+
+    let numeroHtml = '';
+
+    for(let i=0;i<etapa.numeros;i++){
+        if(i===0) {
+            numeroHtml += '<div class="numero pisca"></div>';
+        }else {
+             numeroHtml += '<div class="numero"></div>';
+        }
+       
+    }
+
+    seuVotopara.style.display = 'none';
+    cargo.innerHTML = etapa.titulo;
+    descricao.innerHTML = '';
+    aviso.style.display = 'none';
+    lateral.innerHTML = '';
+    numeros.innerHTML = numeroHtml;
+}
+function atualizaInterface(){
+    let etapa = etapas[etapaAtual];
+    let candidato = etapa.candidatos.filter((item)=>{
+        if(item.numero === numero) {
+            return true;
+        }else {
+            return false;
+        }
+    });
+    if(candidato.length > 0) {
+        candidato = candidato[0];
+        seuVotopara.style.display = 'block';
+        aviso.style.display = 'block';
+        descricao.innerHTML = `Nome: ${candidato.nome}<br/>Partido: ${candidato.partido}`;
+
+        let fotoshtml = '';
+
+        for(let i in candidato.fotos) {
+            fotoshtml += `<div class="d-1-image"><img src="imagens/${candidato.fotos[i].url}" alt=""/>${candidatos.fotos[i].legenda}</div>`
+        }
+
+
+        lateral.innerHTML = fotosHtml;
+    }
+
+}
+
+function clicou(n) {
+    let elnumero = document.querySelector('.numero.pisca');
+    if(elnumero !== null) {
+        elnumero.innerHTML = n;
+        numero = `${numero}${n}`;
+
+        elnumero.classList.remove('pisca');
+        if(elnumero.nextElementSibling !== null) {
+             elnumero.nextElementSibling.classList.add('pisca');
+        }else {
+            atualizaInterface();
+        }
+    }
+}
+
+function branco() {
+    alert('BRANCO')
+
+}
+function corrige() {
+    alert('CORRIGE')
+}
+function confirma() {
+    alert('CONFIRMA')
+}
+
+comecarEtapa();
